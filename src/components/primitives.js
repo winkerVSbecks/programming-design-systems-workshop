340c0f06096e8cdb4926482f549879a241559002
Varun Vachhar <varunvachhar@gmail.com>
2018-04-29 10:17:54 -0400

    📝

---
+++ src/components/primitives.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import * as Spectacle from 'spectacle';
+import styled from 'react-emotion';
+import { compose, defaultProps } from 'recompose';
+import withType from './with-type';
+
+export const Heading = compose(
+  defaultProps({
+    lh: 'title',
+    margin: '0 0 2rem 0',
+    size: 3,
+    textColor: 'secondary',
+    textAlign: 'left',
+  }),
+  withType,
+)(Spectacle.Heading);
+
+export const Text = compose(
+  defaultProps({
+    lh: 'copy',
+    textColor: 'secondary',
+    textAlign: 'left',
+    fw: 5,
+  }),
+  withType,
+)(Spectacle.List);
+
+export const List = styled(Spectacle.List)`
+  list-style: none;
+
+  & li:before {
+    content: ${props => (props.marker ? `'${props.marker}'` : '"- "')};
+    font-weight: 500;
+  }
+`;
+
+export const ListItem = compose(
+  defaultProps({
+    lh: 'copy',
+    textColor: 'secondary',
+    textAlign: 'left',
+    fw: 4,
+  }),
+  withType,
+)(Spectacle.ListItem);
+
+export const Emoji = ({ v, ...props }) => (
+  <span aria-hidden="true" {...props}>
+    {v}
+  </span>
+);
--- src/slides/intro.js
+++ src/slides/intro.js
@@ -1,35 +1,24 @@
 import React from 'react';
-import { SlideSet, Slide, Link, Image } from 'spectacle';
+import { SlideSet, Slide, Image } from 'spectacle';
 
-import { Heading, Text } from 'components';
+import { Heading, Text, List, ListItem, Emoji } from 'components';
 import images from 'images';
 
 export const introSlides = (
   <SlideSet>
     <Slide>
       <Heading f="headline" margin="0">
-        Programming 📐✏️<br />Design Systems
+        Programming <Emoji v="📐✏️" />
+        <br />Design Systems
       </Heading>
     </Slide>
     <Slide>
-      <Heading margin="0 0 1rem 0">
-        Varun Vachhar{' '}
-        <span role="img" aria-label="wave">
-          👋🏽
-        </span>
-      </Heading>
-
-      <Text f={2} margin="0 0 4rem 0">
-        Tech Lead at League Inc.
-      </Text>
-      <Text f={2} margin="0" fw={4}>
-        @winkerVSbecks
-      </Text>
-      <Text f={2} fw={4}>
-        <Link margin="0" textAlign="left">
-          varun.ca
-        </Link>
-      </Text>
+      <Heading f={1}>Overview</Heading>
+      <List marker="+ ">
+        <ListItem>Design Constraints</ListItem>
+        <ListItem>Design System Components</ListItem>
+        <ListItem>Slicing Components</ListItem>
+      </List>
     </Slide>
     <Slide bgColor="secondary">
       <Image src={images.airbnb} />
